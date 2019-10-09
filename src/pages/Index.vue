2218a03ad23356884c8f797f1e87a9a272a94b8a
kristjanjansen <kristjan.jansen@gmail.com>
2019-10-09 19:28:18 +0300

    Cleanup

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,29 +1,26 @@
 <template>
   <Layout>
-
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
+    <!--g-image alt="Example image" src="~/favicon.png" width="135" /-->
 
     <h1>Hello, world!</h1>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores</p>
+    <pre>{{ $page.posts.edges[0].node  }}</pre>
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <div v-for="({ node }) in $page.posts.edges" :key="node.id">
+      <h2>{{ node.title }}</h2>
+      <div v-html="node.content" />
+    </div>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: "Hello, world!"
   }
-}
+};
 </script>
 
 <style>
@@ -31,3 +28,16 @@ export default {
   margin-right: 1rem;
 }
 </style>
+
+<page-query>
+query {
+  posts: allPost {
+    edges {
+      node {
+        title
+        content
+      }
+    }
+  }
+}
+</page-query>
